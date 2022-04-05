2804a0e022276def620c551f97ff99b19da2ed94
rrrx-4 <69399883+rrrx-4@users.noreply.github.com>
2022-04-05 19:58:32 +0530

    change2

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 // import { BACKEND_URL } from '../constant';
 
 const axiosInstance = axios.create({
-    baseURL: "https://main.d3vh9ecp39j6v8.amplifyapp.com"
+    baseURL: "http://localhost:5000/"
 });
 
 export default axiosInstance;
